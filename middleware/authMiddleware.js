aa83df4cfa446553127d1f3c2ec6af4716db77fe
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2025-01-13 15:51:02 +0530

    adding the auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,35 +1,40 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 // const secretKey = 'Rahman@1234';
-const httpStatusCode = require('../constant/httpStatusCode');
+const httpStatusCode = require("../constant/httpStatusCode");
 
 async function getToken(user) {
-    const token = await jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return token;
+  const token = await jwt.sign({ user }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
 }
 
 async function verifyToken(req, res, next) {
-    const token = req.headers.authorization;
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res
+      .status(httpStatusCode.UNAUTHORIZED)
+      .json({ success: false, message: "Unauthorized: Token not provided" });
+  }
 
-    if (!token) {
-        return res.status(httpStatusCode.UNAUTHORIZED).json({ success: false, message: 'Unauthorized: Token not provided' });
-    }
-
-    try {
-        // Split the authorization header by space and directly use the token
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded.user;
-        console.log(req.user)
-        console.log("token:", token);
-        console.log("secreate key:",process.env.JWT_SECRET)
-        next();
-    } catch (error) {
-        console.error('Error verifying token:', error);
-        return res.status(httpStatusCode.UNAUTHORIZED).json({ success: false, message: 'Unauthorized: Invalid token' });
-    }
+  try {
+    // Split the authorization header by space and directly use the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    console.log(req.user);
+    // console.log("token:", token);
+    // console.log("secreate key:",process.env.JWT_SECRET)
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+    return res
+      .status(httpStatusCode.UNAUTHORIZED)
+      .json({ success: false, message: "Unauthorized: Invalid token" });
+  }
 }
 
 module.exports = {
-    getToken,
-    verifyToken
-}
+  getToken,
+  verifyToken,
+};
